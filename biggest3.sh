668bb66041f5406b10fa869face91738609fe503
Sagar1895 <sagarco966@gmail.com>
2022-03-15 06:50:12 +0000

    added automation scripts

---
+++ biggest3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+num1=1
+num2=4
+num3=6
+if [ $num1 -gt $num2 -a $num1 -gt $num3 ]
+then echo "$num1 is greater"
+elif [ $num2 -gt $num1 -a $num2 -gt $num3 ]
+then echo " $num2 is greater" 
+else
+	echo "$num3 is greater"
+fi
